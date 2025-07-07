9a82841453bdcb188f3c9bc520d64e536d4b6d63
SANCHIT KHATE <khatesanchit10022001@gmail.com>
2025-07-07 23:41:02 +0530

    Fix: dynamic short URL using req.headers.host

--- Controllers/url.js
+++ Controllers/url.js
@@ -5,7 +5,7 @@ export const shortUrl = async (req,res)=>{
  const longUrl = req.body.longUrl;
  const shortCode = shortid.generate();
 
- const shortUrl = `http://localhost:1000/${shortCode}`
+ const shortUrl = `http://${req.headers.host}/${shortCode}`;
 
  // save to database
  const newUrl = new Url({shortCode,longUrl})
